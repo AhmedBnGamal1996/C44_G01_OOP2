e6a775c393fc2bd7ba42e545b93ffe7adce45943
Ahmed Gamal <AhmedBnGamal2018@gmail.com>
2025-07-11 09:51:18 +0300

    Video 1 Enacpsulation

---
+++ C44_G01_OOP2_Demo/Encapsulation/Employee.cs
@@ -0,0 +1,87 @@
+﻿
+namespace C44_G01_OOP2_Demo.Encapsulation
+{
+    internal struct Employee
+    {
+        #region Attributes
+        public int empName;
+        private string? Name;
+        #endregion
+
+
+        // Setters and Getters
+        public void SetName(string name)
+        {
+            Name = name.Length > 10 ? Name?.Substring(0,10) : name;
+        }
+
+        public string? GetName()
+        {
+            return Name;
+        }
+
+        // Properties
+        // 1] Automatic Property , 2] Full Property , 3] Indexer
+
+        private decimal salary;
+
+        //Full Property 
+        public decimal Salary 
+        {
+            get { return salary; }
+            set { salary = value < 1000 ? 1000 : value; }  // if not validation == Automatic Property
+        }
+
+
+        //Automatic Property
+
+        public int age { get; set; } = 18; // default value
+
+        // private decimal deduction;       // Not used 
+        public decimal Deduction    // Derived Attribute
+        {
+            get { return 0.1m * salary ; }
+        }
+
+
+
+
+
+
+
+
+
+        #region Constructor
+
+        public Employee(int id, string name, decimal salary)
+        {
+            empName = id;
+            Name = name;
+            Salary = salary;
+        }
+        #endregion
+
+
+        #region Methods
+        public override string ToString()
+        {
+            return $"Id: {empName}, Name: {Name}, Salary: {Salary}" ;
+        }
+
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- C44_G01_OOP2_Demo/Program.cs
+++ C44_G01_OOP2_Demo/Program.cs
@@ -1,10 +1,56 @@
-﻿namespace C44_G01_OOP2_Demo;
+﻿using C44_G01_OOP2_Demo.Encapsulation;
+
+namespace C44_G01_OOP2_Demo;
 internal class Program
 {
      static void Main(string[] args)
     {
 
+        #region Video 1 Enacpsulation 
 
+        #region  Enacpsulation [ Employee ex ] , Properties [Automatic Prop , Full Prop]
+        //Employee employee;
+        //// Varible [ Value Type ]
+
+        //employee = new Employee(1, "Mariam", 1000);
+
+        //Console.WriteLine(employee);
+        //employee.Id = -1000; // Validation is not 
+        //Console.WriteLine(employee.Id);
+
+        //Employee emp1 = new Employee(1, "Mariam", 1000);
+
+        //emp1.SetName("Mariam Shindy");
+
+        //Console.WriteLine(emp1.GetName());
+        //Console.WriteLine(emp1);
+
+        //Employee employee = new Employee(1, "Mariam", 1000);
+
+        //// Console.WriteLine(employee.empName);
+        //employee.Salary = 2000;
+        //Console.WriteLine(employee.Salary);
+
+
+
+        #endregion
+
+
+        #endregion
+
+
+
+        #region  Video 2 
+
+
+
+
+
+
+
+
+
+        #endregion
 
 
 
